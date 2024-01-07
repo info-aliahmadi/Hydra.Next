31a326a9a79cfa5e639bc0f9987e29fa3504256e
info.aliahmadi <info.aliahmadi@gmail.com>
2024-01-07 06:08:21 +0330

    fix route bug*

--- app/dashboard/_lib/routes.js
+++ app/dashboard/_lib/routes.js
@@ -2,7 +2,7 @@
 import authRoutes from '@dashboard/(auth)/_lib/routes';
 import cmsRoutes from '@dashboard/(cms)/_lib/routes';
 import crmRoutes from '@dashboard/(crm)/_lib/routes';
-import crmRoutes from '@dashboard/(sale)/_lib/routes';
+import saleRoutes from '@dashboard/(sale)/_lib/routes';
 import fileStorageRoutes from '@dashboard/(filestorage)/_lib/routes';
 import settingsRoutes from '@dashboard/(settings)/_lib/routes';
 
